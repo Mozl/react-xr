ad11eefdb7c9674b478c0ed838cdf4350cd04f31
Mozl <moselhil@gmail.com>
2022-01-22 23:06:43 +0000

    quick setup

--- src/AR.jsx
+++ src/AR.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import { Suspense } from "react";
-import { Canvas, useThree, extend } from "@react-three/fiber";
-import { OrbitControls, Box } from "@react-three/drei";
+// import { Canvas, useThree, extend } from "@react-three/fiber";
+// import { OrbitControls } from "@react-three/drei";
 import { ARCanvas } from "@react-three/xr";
 // import { ARButton } from 'three/examples/jsm/webxr/ARButton';
 
 // extend({ ARButton });
+
+function Box({ color, size, scale, children, ...rest }) {
+  return (
+    <mesh scale={scale} {...rest}>
+      <boxBufferGeometry attach="geometry" args={size} />
+      <meshPhongMaterial attach="material" color={color} />
+      {children}
+    </mesh>
+  );
+}
 
 const AR = () => {
   return (
@@ -21,11 +31,13 @@ const AR = () => {
             }),
           }}
         >
-          <OrbitControls />
+          {/* <OrbitControls /> */}
           <ambientLight intensity={1} />
           <Suspense fallback={null}>
-            <Box width={10} height={10} depth={10} />
-            {/* <ARButton /> */}
+            <Box color="red" size={[1, 1, 1]} />
+            {/* <Sphere>
+              <meshBasicMaterial attach="material" color="hotpink" />
+            </Sphere> */}
           </Suspense>
         </ARCanvas>
       </div>
