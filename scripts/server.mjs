a5b5b5ef7038cf4e3c852f6ed1a1581a26a0f85e
Mozl <moselhil@gmail.com>
2022-02-08 13:14:56 +0000

    show user and websocket boxes, add id

--- scripts/server.mjs
+++ scripts/server.mjs
@@ -2,21 +2,21 @@ import { WebSocketServer } from "ws";
 import { v4 as uuid } from "uuid";
 
 const wss = new WebSocketServer({ port: 8080 });
-const idMap = [];
+// const idMap = [];
 
-wss.uuid = uuid();
-idMap.push(wss.uuid, wss);
+// wss.uuid = uuid();
+// idMap.push(wss.uuid, wss);
 
 wss.on("connection", function connection(ws) {
+  ws.id = uuid();
+
+  wss.clients.forEach(function each(client) {
+    console.log("Client.ID: " + client.id);
+  });
   ws.on("message", function message(info) {
-    ws.id = uuid();
     try {
-      // console.log(info.toString("utf8"));
-      info = JSON.parse(info);
-      if ((info.action = "send")) {
-        console.log(info);
-        ws.send(info);
-      }
+      console.log('info.toString("uft8"): ', info.toString("utf8"));
+      ws.send(info.toString("utf8"));
     } catch (e) {
       console.log("Error processing incoming message", e);
     }
--- src/AR.jsx
+++ src/AR.jsx
@@ -2,53 +2,34 @@ import React, { useEffect, useState } from "react";
 import { Suspense } from "react";
 import { ARCanvas } from "@react-three/xr";
 import HitTestReticle from "./HitTestReticle";
+import { v4 as uuid } from "uuid";
 
+const ws = new WebSocket("ws://localhost:8080");
 const AR = () => {
   const [reticlePosition, setReticlePosition] = useState([]);
   const [objectList, setObjectList] = useState([]);
   console.log("objectList: ", objectList);
   const [wsObjectList, setWsObjectList] = useState([]);
-  const ws = new WebSocket("ws://localhost:8080");
+  console.log("wsObjectList: ", wsObjectList);
+  const id = uuid();
 
-  useEffect(() => {
-    setObjectList(
-      objectList.concat(
+  ws.onmessage = ({ data }) => {
+    console.log("data: ", data);
+    console.log("data from ws: ", JSON.parse(data));
+    data = JSON.parse(data);
+    const positionFromWs = [data.x * 1.3, data.y + 0.7, data.z];
+    console.log("positionFromWs: ", positionFromWs);
+
+    setWsObjectList(
+      wsObjectList.concat(
         <>
-          <mesh position={[0, -1.6, -3.3]}>
-            <boxGeometry args={[0.5, 0.5, 0.5]} />
-            <meshBasicMaterial />
-          </mesh>
-        </>,
-        <>
-          <mesh position={[-1, -1.6, -3.3]}>
-            <boxGeometry args={[0.5, 0.5, 0.5]} />
-            <meshBasicMaterial />
-          </mesh>
-        </>,
-        <>
-          <mesh position={[1, -1.6, -3.3]}>
-            <boxGeometry args={[0.5, 0.5, 0.5]} />
-            <meshBasicMaterial />
+          <mesh position={positionFromWs}>
+            <boxGeometry args={[0.7, 0.5, 0.5]} />
+            <meshBasicMaterial color={"red"} />
           </mesh>
         </>
       )
     );
-  }, []);
-
-  ws.onmessage = (event) => {
-    console.log("event: ", event);
-    // setObjectList(JSON.parse(event.data).data);
-    console.log("parsed message from ws server:", JSON.parse(event));
-    // setWsObjectList(
-    //   wsObjectList.concat(
-    //     <>
-    //       <mesh position={reticlePos}>
-    //         <boxGeometry args={[0.5, 0.5, 0.5]} />
-    //         <meshBasicMaterial />
-    //       </mesh>
-    //     </>
-    //   )
-    // );
   };
 
   // listen onmessage for updates from websocket
@@ -57,17 +38,24 @@ const AR = () => {
   // render each array of coordinates, rerender if array is updated
 
   const handleSelect = (reticlePos) => {
+    console.log("reticlePos: ", reticlePos);
+    const objToSend = {
+      ...reticlePos,
+      id,
+    };
+    ws.send(JSON.stringify(objToSend));
     setObjectList(
       objectList.concat(
         <>
           <mesh position={reticlePos}>
             <boxGeometry args={[0.5, 0.5, 0.5]} />
-            <meshBasicMaterial />
+            <meshBasicMaterial color={"blue"} />
           </mesh>
         </>
       )
     );
   };
+
   return (
     <>
       <div className="three">
